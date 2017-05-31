d96b40cdc5657abafacdd3e851029a1b2cad5e6a
Administrator <Administrator@RO41MA8JR3ILJSG>
2017-05-31 11:40:35 +0800

    friday

--- WebApplication9/Models/Move.cs
+++ WebApplication9/Models/Move.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication9.Models
 {
     public class Move
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
         public DateTime ReleaseDate { get; set; }
         public string Genre { set; get; }
         public decimal Price { get; set; }
+        [Range(1,5)]
+        public int rating { get; set; }
     }
 }
