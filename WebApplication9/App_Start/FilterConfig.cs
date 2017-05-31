bfa18f415775c314b92938ac6121dbdccc14583b
Administrator <Administrator@RO41MA8JR3ILJSG>
2017-05-31 15:37:48 +0800

    3

--- WebApplication9/App_Start/FilterConfig.cs
+++ WebApplication9/App_Start/FilterConfig.cs
@@ -13,8 +13,8 @@ namespace WebApplication9
         {
             //filters.Add(new HandleErrorAttribute());
             //注册自定义Action过滤器，优先级最低，最高的为在Action中设置其过滤器
-            filters.Add(new MyActionFilterAttribute() { Name = "测试在所有的Action中加入自定义过滤器" });
-            filters.Add(new MyExceitionFilterAttribute());
+           // filters.Add(new MyActionFilterAttribute() { Name = "测试在所有的Action中加入自定义过滤器" });
+           // filters.Add(new MyExceitionFilterAttribute());
         }
     }
 }
