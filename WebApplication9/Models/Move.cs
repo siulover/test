1c3111da0ec53fce133fee94f682b601164b9803
Administrator <Administrator@RO41MA8JR3ILJSG>
2017-06-06 09:26:23 +0800

    2

--- WebApplication9/Models/Move.cs
+++ WebApplication9/Models/Move.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication9.Models
 {
